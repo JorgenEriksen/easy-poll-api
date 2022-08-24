465ebfa77d4961bfae64dd10bd187ad95f920a51
Jorgen Eriksen <jrgn93@gmail.com>
2022-08-24 09:38:48 +0200

    init

---
+++ EasyPollAPI/Controllers/PollGameController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EasyPollAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PollGameController : ControllerBase
+    {
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+"# EasyPollAPI" 
